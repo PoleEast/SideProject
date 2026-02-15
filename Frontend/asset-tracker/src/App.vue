5322dee27ce6b485f4285aaac419a63a6afdc71c
PoleEast <a1525123@gmail.com>
2026-02-15 21:02:25 +0800

    init: 建立 Vue 3 前端專案與基礎 Layout

---
+++ Frontend/asset-tracker/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <n-message-provider>
+    <n-dialog-provider>
+      <MainLayout />
+    </n-dialog-provider>
+  </n-message-provider>
+</template>
+
+<script setup lang="ts">
+import MainLayout from './components/MainLayout.vue'
+</script>
---
+++ Frontend/asset-tracker/src/components/MainLayout.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import {
+  NFlex,
+  NImage,
+  NLayout,
+  NLayoutContent,
+  NLayoutHeader,
+  NLayoutSider,
+  NText,
+} from 'naive-ui'
+import UserButton from './UserButton.vue'
+import { RouterView } from 'vue-router'
+import LoginModal from './LoginModal.vue'
+</script>
+
+<template>
+  <n-layout>
+    <n-layout-header bordered class="px-5">
+      <n-flex justify="space-between" align="center">
+        <n-text>
+          <n-flex align="center">
+            <n-image width="50" src="/logoLight.svg" />
+            <h3>TrackFin</h3>
+          </n-flex>
+        </n-text>
+        <User-button />
+      </n-flex>
+    </n-layout-header>
+    <n-layout-sider></n-layout-sider>
+    <n-layout-content>
+      <LoginModal />
+      <router-view />
+    </n-layout-content>
+  </n-layout>
+</template>
